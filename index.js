8876249a58a6c9a3fdb22b8a6084758b24caad7a
Philipp Storz <philipp.storz@t-online.de>
2021-06-29 23:14:26 +0200

    update

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const cors = require("cors");
+import http from "http";
 
-
-app.use(cors())
-
-
-
-app.get("", function (req, res) {
-  res.send("Test");
-});
-
-
-
-//        HTTP Connection
-const host = '0.0.0.0';
+const host = "0.0.0.0";
 const port = 4000;
 
-const server = app.listen(port, host, () => {
-  console.log('Listening on port %s', server.address().port)
-})
 
 
+const server = http.createServer(http.createServer(function (req, res) {
+  res.end("Test");
+}));
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
