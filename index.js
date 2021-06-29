2b316d6f69f4dc03c5b8f0f3f547cbdf78218891
Philipp Storz <philipp.storz@t-online.de>
2021-06-30 00:05:45 +0200

    update

--- index.js
+++ index.js
@@ -1,53 +1,14 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const https = require("https")
-const fs = require("fs")
-const path = require("path")
+const http = require("http");
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-app.get("/online", function (req, res) {
-  const obj = {
-    status: "online",
-  };
-  res.send(JSON.stringify(obj));
-});
-
-app.post("/payment", async (req, res) => {
-  let { amount, id, tokens, uid, bill } = req.body;
-  try {
-    const payment = await stripe.paymentIntents.create({
-      amount,
-      currency: "USD",
-      description: bill + "/" + uid,
-      payment_method: id,
-      confirm: true,
-    });
-    console.log("Payment", payment);
-    res.json({
-      message: "Payment successful",
-      success: true,
-    });
-  } catch (error) {
-    console.log("Error", error);
-    res.json({
-      message: "Payment failed",
-      success: false,
-    });
-  }
-});
-
-//        HTTP Connection
-const host = '0.0.0.0';
+const host = "0.0.0.0";
 const port = 4000;
 
-const server = app.listen(port, host, () => {
-  console.log('Listening on port %s', server.address().port)
-})
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end("Hello world");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
